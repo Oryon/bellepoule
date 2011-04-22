2c80c737a649a6e819cf8651ea7415f57aa0bfe3
betonniere <>
2011-04-22 12:10:25 +0200

    Correction de la regression proposant l'impression du tableau et des feuilles de matchs en même temps.

--- sources/version.h
+++ sources/version.h
@@ -3,7 +3,7 @@
 
 #define VERSION           "2"
 #define VERSION_REVISION  "0"
-#define VERSION_MATURITY  "beta4"
+#define VERSION_MATURITY  "beta5"
 #define VERSION_DAY       "19"
 #define VERSION_MONTH     "12"
 
