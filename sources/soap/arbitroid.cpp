050584a621532602f57db1af7b6dcd19b5f30334
betonniere <>
2011-10-07 12:10:19 +0200

    Soap experimentations.

--- sources/soap/arbitroid.cpp
+++ sources/soap/arbitroid.cpp
@@ -10,19 +10,59 @@ const char server[] = "http://127.0.0.1:8080";
 int main (int argc, char *argv[])
 {
   BellePouleProxy belle_poule;
-  int             status;
 
   belle_poule.soap_endpoint = server;
 
-  if (belle_poule.SetMatchResult (111u,
-                                  222u,
-                                  333u,
-                                  10u,
-                                  20u,
-                                  11u,
-                                  21u,
-                                  status) != SOAP_OK)
+  // Competition data
   {
-    belle_poule.soap_stream_fault (std::cerr);
+    char *competition_data;
+
+    if (belle_poule.GetCompetitionData (111u,
+                                        "club",
+                                        competition_data) != SOAP_OK)
+    {
+      belle_poule.soap_stream_fault (std::cerr);
+    }
+    else
+    {
+      printf ("=== > %s\n", competition_data);
+    }
+  }
+
+  // Player data
+  {
+    char *player_data;
+
+    if (belle_poule.GetPlayerData (111u,
+                                   35830,
+                                   "club",
+                                   player_data) != SOAP_OK)
+    {
+      belle_poule.soap_stream_fault (std::cerr);
+    }
+    else
+    {
+      printf ("=== > %s\n", player_data);
+    }
+  }
+
+  // Match result
+  {
+    soap2__MatchResult match_result;
+    int                status;
+
+    match_result.fencerAId = 888;
+    match_result.fencerBId = 777;
+    match_result.fencerAScore = 15;
+    match_result.fencerBScore = 9;
+
+    if (belle_poule.SetMatchResult (111u,
+                                    222u,
+                                    333u,
+                                    &match_result,
+                                    status) != SOAP_OK)
+    {
+      belle_poule.soap_stream_fault (std::cerr);
+    }
   }
 }
--- sources/soap/belle_poule.cpp
+++ sources/soap/belle_poule.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdsoap2.h>
 
-#include "soapBellePouleProxy.h"
 #include "soapBellePouleService.h"
 #include "BellePoule.nsmap"
 
@@ -13,13 +12,30 @@ int main (int argc, char **argv)
   return 0;
 }
 
-int BellePouleService::SetMatchResult (unsigned int  competition_id,
-                                       unsigned int  round_id,
-                                       unsigned int  match_id,
-                                       unsigned int  fencerA_id,
-                                       unsigned int  fencerB_id,
-                                       unsigned int  fencerA_score,
-                                       unsigned int  fencerB_score,
+// --------------------------------------------------------------------------------
+int BellePouleService::GetCompetitionData (unsigned int   CompetitionId,
+                                           char          *DataName,
+                                           char         *&competition_data)
+{
+  competition_data = "Championnat de Bretagne";
+  return SOAP_OK;
+}
+
+// --------------------------------------------------------------------------------
+int BellePouleService::GetPlayerData (unsigned int   CompetitionId,
+                                      unsigned int   PlayerId,
+                                      char          *DataName,
+                                      char         *&player_data)
+{
+  player_data = "Le Roux";
+  return SOAP_OK;
+}
+
+// --------------------------------------------------------------------------------
+int BellePouleService::SetMatchResult (unsigned int        competition_id,
+                                       unsigned int        round_id,
+                                       unsigned int        match_id,
+                                       soap2__MatchResult *result,
                                        int                &status)
 {
   status = 0;
@@ -27,10 +43,10 @@ int BellePouleService::SetMatchResult (unsigned int  competition_id,
   printf ("competition_id %d\n", competition_id);
   printf ("round_id       %d\n", round_id);
   printf ("match_id       %d\n", match_id);
-  printf ("fencerA_id     %d\n", fencerA_id);
-  printf ("fencerB_id     %d\n", fencerB_id);
-  printf ("fencerA_score  %d\n", fencerA_score);
-  printf ("fencerB_score  %d\n", fencerB_score);
+  printf ("fencerA_id     %d\n", result->fencerAId);
+  printf ("fencerB_id     %d\n", result->fencerBId);
+  printf ("fencerA_score  %d\n", result->fencerAScore);
+  printf ("fencerB_score  %d\n", result->fencerBScore);
 
   return SOAP_OK;
 }
