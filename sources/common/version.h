63edbce73804052d90629118ae8780219be7c71e
betonniere <>
2013-10-21 21:15:56 +0200

    alpha8

--- sources/common/version.h
+++ sources/common/version.h
@@ -4,7 +4,7 @@
 #define VERSION_BRANCH    "UNSTABLE"
 #define VERSION           "4"
 #define VERSION_REVISION  "0"
-#define VERSION_MATURITY  "alpha7"
+#define VERSION_MATURITY  "alpha8"
 #define VERSION_DAY       "20"
 #define VERSION_MONTH     "12"
 
